7a3795985af4ddf55ef6a60115ff9b12c6ba15ca
elroi99 <61104879+elroi99@users.noreply.github.com>
2023-01-29 10:13:47 +0000

    second commit

---
+++ level_1/second.java
@@ -0,0 +1,10 @@
+class second {
+  public static void main(String[] args) {
+    System.out.println("This is second!");
+    add(2, 3);
+  }
+
+  public static void add(int a, int b) {
+    System.out.println(a + b);
+  }
+}
---
+++ snippet.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+// running the program
+// javac second.java
+// java second.java
